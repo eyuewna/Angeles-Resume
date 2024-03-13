17827a6a537a942e41b2585bb0f61011f05ac4cf
Euna Angeles <202210630@fit.edu.ph>
2024-03-13 21:16:10 +0800

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,21 @@
-a
+$(document).ready(function(){
+
+    $('#profile_ripple').ripples({
+        resolution:512,
+        dropRadius:10
+    });
+
+    const bars =document.querySelectorAll('.progress_bar');
+
+
+    bars.forEach(function(bar){
+        let percentage = bar.dataset.percent;
+        let tooltip =bar.children[0];
+        tooltip.innerText =percentage +'%';
+        bar.style.width=percentage +'%';
+        console.log(percentage);
+
+    })
+
+
+})
